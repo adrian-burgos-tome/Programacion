da30d1fc7bc034b0c09968d736b7b543014c37f2
adrian-burgos-tome <aburtom0312@g.educaand.es>
2025-10-17 17:54:40 +0200

    Creación de ficheros y fizz buzz en java

---
+++ java/Fizz_Buzz.java
@@ -0,0 +1,16 @@
+    void main(){
+        for(int i=0; i<=100; i++){
+        if (i % 3 == 0 && i % 5 == 0){
+            System.out.println("fizzbuzz");
+        }
+        else if (i % 3 == 0) {
+            System.out.println("fizz");
+        }
+        else if (i % 5 == 0) {
+            System.out.println("buzz");
+        }
+        else {
+            System.out.println(i);
+        }
+        }
+    }
